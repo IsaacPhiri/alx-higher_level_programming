88b3aa266b0596e2a08b95b6a9acace895f26b4f
Isaac Phiri <isaacphiri315@gmail.com>
2022-08-24 02:18:39 -0700

    Divided by -10 to find the last digit of a negative number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-    number = abs(number)
-
-last_digit = (number) % 10
+    last_digit = number % -10
+else:
+    last_digit = number % 10
 
 if last_digit > 5:
     print(f"Last digit of {number:d} is {last_digit:d} and is greater than 5")
