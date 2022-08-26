dcff3555e3d3fac281ec6f6cbc5e16b69d3720a1
Isaac Phiri <isaacphiri315@gmail.com>
2022-08-26 06:04:00 -0700

    Added a program that prints command line arguments passed

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    """Program to find number of args
+    """
+
+    n = len(sys.argv) - 1
+
+    if n == 0:
+        print("{} arguments.".format(n))
+    elif n == 1:
+        print("{} argument:".format(n))
+        print("{}: {}".format(n, sys.argv[1]))
+    elif n > 1:
+        print("{} arguments:".format(n))
+        for i in range(1, n + 1):
+            print("{}: {}".format(i, sys.argv[i]))
+            i += 1
+
+
+if __name__ == '__main__':
+    main()
