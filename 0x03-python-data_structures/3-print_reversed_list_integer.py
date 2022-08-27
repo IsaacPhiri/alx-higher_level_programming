13138efd1d3c18c0ff054cd4e6bcb12beb30ee70
Isaac Phiri <isaacphiri315@gmail.com>
2022-08-27 05:16:02 -0700

    Added a function that reverses a list

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# create a var to hold the reversed string
+# use a for loop to print out the elements of the reversed list
+
+def print_reversed_list_integer(my_list=[]):
+    """Reversed list
+    """
+
+    rev_list = my_list[::-1]
+
+    for i in range(0, len(rev_list)):
+        print("{}".format(rev_list[i]))
