63b7cf85084f7215c362eeebb86bec335ff253a3
Isaac Phiri <isaacphiri315@gmail.com>
2022-08-27 05:38:00 -0700

    Added format code for ints

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -7,6 +7,6 @@ def print_reversed_list_integer(my_list=[]):
     """Reversed list
     """
 
-    my_list = my_list[::-1]
-    for i in my_list:
-        print("{}".format(i))
+    rev_list = my_list[::-1]
+    for i in range(0, len(rev_list)):
+        print("{:d}".format(rev_list[i]))
