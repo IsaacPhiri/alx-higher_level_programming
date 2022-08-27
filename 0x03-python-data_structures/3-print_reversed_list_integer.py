efd4dce41c70b0217e9d774eb64f05fb6f98b281
Isaac Phiri <isaacphiri315@gmail.com>
2022-08-27 05:25:12 -0700

    Used my_list instead of range and len function

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -8,5 +8,5 @@ def print_reversed_list_integer(my_list=[]):
     """
 
     my_list = my_list[::-1]
-    for i in range(0, len(my_list)):
-        print("{}".format(my_list[i]))
+    for i in my_list:
+        print("{}".format(i))
