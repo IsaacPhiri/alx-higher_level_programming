ee46918673732669302180687aa895ef4d7aca74
Isaac Phiri <isaacphiri315@gmail.com>
2022-08-27 05:21:04 -0700

    Mutated my_list to a reversed list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -7,7 +7,6 @@ def print_reversed_list_integer(my_list=[]):
     """Reversed list
     """
 
-    rev_list = my_list[::-1]
-
-    for i in range(0, len(rev_list)):
-        print("{}".format(rev_list[i]))
+    my_list = my_list[::-1]
+    for i in range(0, len(my_list)):
+        print("{}".format(my_list[i]))
