0fcf8e15830a8c55c75b9e9e7d3ccdd464dd6e2d
Isaac Phiri <isaacphiri315@gmail.com>
2022-08-27 23:47:30 -0700

    Added a function that prints elements of a 2D matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# use a loop to iterate the main list
+# use another loop to iterate the lists in the main list
+# print out the element in the inner loop
+# increment to access the next element of the main list
+# print out elements
+
+def print_matrix_integer(matrix=[[]]):
+    """print matrix
+    """
+
+    #for i in range(0, len(matrix)):
+     #   print("{}".format(matrix[i]))
+
+    print('\n'.join(map(''.join, matrix)))
