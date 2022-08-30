bef020f317b74b97f5393be7181bffa9e9c4708c
Isaac Phiri <isaacphiri315@gmail.com>
2022-08-30 04:54:10 -0700

    Added a program that imports a variable from another file

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from variable_load_5 import a
+
+
+def main():
+    """a program that imports the variable
+    a from the file variable_load_5.py
+    """
+
+    print("{}".format(a))
+
+
+if __name__ == '__main__':
+    main()
