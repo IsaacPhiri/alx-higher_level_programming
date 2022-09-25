6cf76bf724893bbbb8a7bb815588e77f74775d27
Isaac Phiri <isaacphiri315@gmail.com>
2022-09-25 06:23:00 -0700

    Edited type conversion

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,45 @@
 #!/usr/bin/python3
 
-def add_integer(a, b=98):
-    """ Add 2 integers """
+"""
+This is a module to add two numbers
+This module performs addition operation between two integers,
+or two floats and raises an exception if the number is not an
+integer or a float.
+"""
 
-    if not isinstance(a, (int, float)):
+
+def add_integer(a, b=98):
+    """Adds two numbers
+    Return the addition of two numbers with spcific validation.
+    Arguments:
+        a (int or float): first value to add
+        b (int or float): second value to add
+    Return:
+        the addition of a and b
+    """
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
 
-    a = int(a)
-    b = int(b)
-
+    a = convert_to_int(a)
+    b = convert_to_int(b)
     return a + b
+
+
+def convert_to_int(num):
+
+    """Casts the data type of the num parameter
+    Converts a float number to an integer
+    Arguments:
+        num(int or float): the number to cast.
+    Returns:
+        int: The number casted to integer
+    """
+
+    if type(num) is float:
+        num = int(num)
+        return num
+
+    return num
