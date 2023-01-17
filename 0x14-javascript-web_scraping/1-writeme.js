04ec33eab9cdca77d778d8ba078e917a8a400dc6
Isaac Phiri <isaacphiri315@gmail.com>
2023-01-17 14:38:59 +0000

    Edited second line

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
+'use strict';
 
-const fs = require("fs");
+const fs = require('fs');
 
 const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
 
-fs.writeFile(filePath, stringToWrite, { encoding: "utf-8" }, (err) => {
+fs.writeFile(filePath, stringToWrite + '\n', { encoding: 'utf-8' }, (err) => {
   if (err) {
     console.log(err);
+    process.exit(1);
   }
+  process.exit(0);
 });
